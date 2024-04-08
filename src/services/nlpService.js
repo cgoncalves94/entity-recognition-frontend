6e93d3ff0904fb0e962fdc07d0cc62119c3152a8
Cesar Augusto Goncalves <goncalves.cesaraugusto94@gmail.com>
2024-04-09 00:54:28 +0100

    Update API URLs in authService.js and nlpService.js

--- src/services/nlpService.js
+++ src/services/nlpService.js
@@ -1,54 +1,54 @@
 // src/services/nlpService.js
-import axios from 'axios';
+import axios from "axios";
 
 /**
  * Service for processing text using NLP (Natural Language Processing).
  * @namespace nlpService
  */
 const nlpService = {
-  /**
-   * Process an array of texts using NLP.
-   * @async
-   * @memberof nlpService
-   * @param {Array<string>} textsArray - The array of texts to be processed.
-   * @param {string} token - The authorization token.
-   * @returns {Promise<any>} A promise that resolves to the processed data.
-   * @throws {Error} If an error occurs during the processing.
-   */
-  processText: async (textsArray, token) => {
-    try {
-      const response = await axios.post(
-        'https://tech-entity-recognition.fly.dev/nlp/process/',
-        { texts: textsArray },
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  },
+    /**
+     * Process an array of texts using NLP.
+     * @async
+     * @memberof nlpService
+     * @param {Array<string>} textsArray - The array of texts to be processed.
+     * @param {string} token - The authorization token.
+     * @returns {Promise<any>} A promise that resolves to the processed data.
+     * @throws {Error} If an error occurs during the processing.
+     */
+    processText: async (textsArray, token) => {
+        try {
+            const response = await axios.post(
+                "http://127.0.0.1:16000/nlp/process/",
+                { texts: textsArray },
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
 
     /**
-   * Match recommendations with blueprints.
-   * @async
-   * @memberof nlpService
-   * @param {Array<Object>} recommendations - The recommendations to match with blueprints.
-   * @param {string} token - The authorization token.
-   * @returns {Promise<any>} A promise that resolves to the matched blueprints data.
-   * @throws {Error} If an error occurs during matching.
-   */
-  matchBlueprints: async (recommendations, token) => {
-    try {
-      const response = await axios.post(
-        'https://tech-entity-recognition.fly.dev/nlp/match-blueprints/',
-        recommendations,
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  },
+     * Match recommendations with blueprints.
+     * @async
+     * @memberof nlpService
+     * @param {Array<Object>} recommendations - The recommendations to match with blueprints.
+     * @param {string} token - The authorization token.
+     * @returns {Promise<any>} A promise that resolves to the matched blueprints data.
+     * @throws {Error} If an error occurs during matching.
+     */
+    matchBlueprints: async (recommendations, token) => {
+        try {
+            const response = await axios.post(
+                "http://127.0.0.1:16000/nlp/match-blueprints/",
+                recommendations,
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
 };
 
 // Export the nlpService
